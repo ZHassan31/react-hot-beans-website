b3954175a2d4cb2e9d1f6e41fd15ccf98f34b406
ZHassan31 <zubayr141@hotmail.com>
2024-08-14 22:43:34 +0100

    minor improvement

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./styles.css";
-
+import Navbar from "./NavBar";
 function Footer() {
+
+    const currentYear = new Date().getFullYear();
+
     return <div className="footer">
-        Footer</div>;
+        <Navbar />
+        <p>©Hot Beans {currentYear} - All rights reserved</p>
+
+    </div>;
 }
 
 export default Footer;
